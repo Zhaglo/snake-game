4f41a18ac6ac5b45894b46f29acbfdc7f357d7af
zhaglo <jagloik@gmail.com>
2025-09-22 17:55:33 +0300

    feat: the first version of the Snake game in C++ (macOS)

---
+++ main.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstdlib>
+
+
+bool gameOver;
+const int width = 20;
+const int height = 10;
+
+int x, y;
+int fruitX, fruitY;
+int score;
+
+const int maxTail = 100;
+int tailX[maxTail], tailY[maxTail];
+int nTail;
+
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+
+// === Функции для неблокирующего ввода ===
+void enableRawMode() {
+    termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+}
+
+void disableRawMode() {
+    termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= (ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+// ========================================
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    std::cout << "\033[2J\033[H";
+    for (int i = 0; i < width + 2; i++) std::cout << "#";
+    std::cout << "\n";
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) std::cout << "#";
+
+            if (j == x && i == y) {
+                std::cout << "O";
+            }
+            else {
+                bool printTail = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        std::cout << "o";
+                        printTail = true;
+                        break;
+                    }
+                }
+                if (!printTail) {
+                    if (j == fruitX && i == fruitY) {
+                        std::cout << "F";
+                    } else std::cout << " ";
+                }
+            }
+
+            if (j == width - 1) std::cout << "#";
+        }
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < width + 2; i++) std::cout << "#";
+    std::cout << "\n";
+    std::cout << "Score: " << score << "\n";
+}
+
+void Input() {
+    char buf = 0;
+    if (read(STDIN_FILENO, &buf, 1) > 0) {
+        switch (buf) {
+            case 'a': dir = LEFT; break;
+            case 'd': dir = RIGHT; break;
+            case 'w': dir = UP; break;
+            case 's': dir = DOWN; break;
+            case 'x': gameOver = true; break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    switch (dir) {
+        case LEFT: x--; break;
+        case RIGHT: x++; break;
+        case UP: y--; break;
+        case DOWN: y++; break;
+        default: break;
+    }
+
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y) {
+            gameOver = true;
+        }
+    }
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        nTail++;
+
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+    }
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+}
+
+int main() {
+    enableRawMode();
+    Setup();
+
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        usleep(10);
+    }
+
+    disableRawMode();
+    return 0;
+}
